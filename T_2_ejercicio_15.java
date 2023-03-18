1be399da248eecd0c5e29eb9610414b6b48d70cb
martinm0n <mmontealegrec@unal.edu.co>
2023-03-18 12:02:45 -0500

    ejercicio 15

---
+++ T_2_ejercicio_15.java
@@ -0,0 +1,43 @@
+
+package t_2_ejercicio_15;
+import java.util.Scanner;
+
+
+public class T_2_ejercicio_15 {
+
+    
+    public static void main(String[] args) {
+        int PESOA=0;
+        int PESOB=0;
+        int PESOC=0;
+        int PESOD=0;
+        
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Ingrese el peso de la esfera A");
+        PESOA = sc.nextInt();
+        System.out.println("Ingrese el peso de la esfera B");
+        PESOB = sc.nextInt();
+        System.out.println("Ingrese el peso de la esfera C");
+        PESOC = sc.nextInt();
+        System.out.println("Ingrese el peso de la esfera D");
+        PESOD = sc.nextInt();
+        
+        
+    }
+            
+        
+        
+        
+        
+        
+            
+        
+        
+        
+                
+            
+        
+            
+        }
+    }
+    
